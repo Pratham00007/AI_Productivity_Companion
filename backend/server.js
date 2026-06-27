052d019754ece8a95ed95ab4f161f3e65756faf6
Pratham <pratham10600@gmail.com>
2026-06-27 15:06:26 +0530

    fixing deployment  errors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://ai-productivity-companio-af14c.web.app"
+  "https://ai-productivity-companio-af14c.web.app",
+  "https://ai-productivity-companio-af14c.firebaseapp.com"
 ];
 
 app.use(cors({
